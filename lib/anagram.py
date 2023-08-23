8463e5ab809baf916f4abe80534932340aa8e59b
whiplade <nmawira13@gmail.com>
2023-08-23 08:13:10 +0300

    Completed Assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+        self.sorted_word = sorted(self.word)
+    
+    def match(self, anagrams):
+        matches = []
+        for anagram in anagrams:
+            if self.word != anagram.lower() and self.sorted_word == sorted(anagram.lower()):
+                matches.append(anagram)
+        return matches
+    
+listen = Anagram("listen")
+result = listen.match(['enlists', 'google', 'inlets', 'banana'])
+print(result)
